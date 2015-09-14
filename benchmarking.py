1b4f329f71e4b650fdb8a857dc8c4620ab36cf34
Aquaj <aquajvalin@gmail.com>
2015-09-15 00:33:27 +0200

    Automatized benchmarking for several parameters to test. Refactored the output text into a function to be more easily modified (especially when adding new metrics)

--- benchmarking.py
+++ benchmarking.py
@@ -3,12 +3,17 @@ from multiprocessing import Process, Lock, Queue, Value
 from multiprocessing.queues import SimpleQueue
 import time
 
+values = [(500, 500), (250, 500), (500, 250), (1500, 1500), (5000, 5000), (1000, 500)]
+
 def countSteps(p):
 	while True:
-		time.sleep(0.1)
 		p.value+=1
-		if p.value%100 == 0:
-			print "\t\t\r"+str(p.value),
+
+def writedata(values):
+	output = ""
+	for v in values:
+		output += "".join([" " for tab in range(8-len(str(v)))])+str(v)
+	return output+"\n"
 
 def degree(G):
 	d = 0.0
@@ -17,30 +22,37 @@ def degree(G):
 	return d/len(G.nodes())
 
 if __name__ == "__main__":
-	Score = networkx.MultiDiGraph()
-	for i in range(1,5000):
-		e = (i, i//2)
-		if e not in Score.edges():
-			Score.add_edges_from([e])
-	# print Score.edges()
+	for analysisV in values:
 
-	V = len(Score.nodes())
-	E = len(Score.edges())
-	D = degree(Score)
+		inp = analysisV[0]
+		out = analysisV[1]
 
-	print " Graph - "+str(V)+" : "+str(E)+" : "+str(D)
-	time = Value('i', 0)
-	p1 = Process(target = countSteps, args=[time])
-	p1.start()
-	networkx.betweenness_centrality(Score)
-	p1.terminate()
-	print "Done."
+		Score = networkx.MultiDiGraph()
+		for i in range(1,5000):
+			e = (i%out, i%inp)
+			if e not in Score.edges():
+				Score.add_edges_from([e])
+		# print Score.edges()
 
-	eset = []
-	for e in Score.edges():
-		eset += e[2]
-	eset = set(eset)
+		oset = set([i for i,o in Score.edges()])
+		oset = set(oset)
+		iset = set([o for i,o in Score.edges()])
+		iset = set(iset)
 
-	data = open("banchmark_data", 'a')
-	data.write(str(V)+"\t"+str(E)+"\t"+str(D)+"\t"+str(len(eset))+"\t"+str(time.value)+"\n")
-	data.close()
+		V = len(Score.nodes())
+		E = len(Score.edges())
+		D = degree(Score)
+		O = len(oset)
+		I = len(iset)
+
+		print " Graph - "+writedata([V,E,D,O,I])
+		time = Value('i', 0)
+		p1 = Process(target = countSteps, args=[time])
+		p1.start()
+		networkx.betweenness_centrality(Score)
+		p1.terminate()
+		print "Done."
+
+		data = open("banchmark_data", 'a')
+		data.write(writedata([V,E,I,O,time.value]))
+		data.close()
