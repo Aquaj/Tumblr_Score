e2f134e1b3428da10b1e5dfe12715fa6f0e6565c
Aquaj <aquajvalin@gmail.com>
2015-09-14 22:25:06 +0200

    Added benchmarking test file for development purposes.

---
+++ benchmarking.py
@@ -0,0 +1,40 @@
+import networkx
+from multiprocessing import Process, Lock, Queue, Value
+from multiprocessing.queues import SimpleQueue
+import time
+
+def countSteps(p):
+	while True:
+		time.sleep(0.1)
+		p.value+=1
+		if p.value%100 == 0:
+			print "\t\t\r"+str(p.value),
+
+def degree(G):
+	d = 0.0
+	for n in G.degree(G.nodes()):
+		d += n
+	return d/len(G.nodes())
+
+if __name__ == "__main__":
+	Score = networkx.MultiDiGraph()
+	for i in range(1,2500):
+		e = (i%2, i%3)
+		if e not in Score.edges():
+			Score.add_edges_from([e])
+
+	V = len(Score.nodes())
+	E = len(Score.edges())
+	D = degree(Score)
+
+	print " Graph - "+str(V)+" : "+str(E)+" : "+str(D)
+	time = Value('i', 0)
+	p1 = Process(target = countSteps, args=[time])
+	p1.start()
+	networkx.betweenness_centrality(Score)
+	p1.terminate()
+	print "Done."
+
+	data = open("banchmark_data", 'a')
+	data.write(str(V)+"\t"+str(E)+"\t"+str(D)+"\t"+str(time.value)+"\n")
+	data.close()
